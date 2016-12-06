34a39446032d22e0a2446d479851940fad082e2d
Yue Wu <yuriyuewu@gmail.com>
2016-12-06 17:25:41 -0500

    Add files via upload

---
+++ WebContent/js/columnchart.js
@@ -0,0 +1,61 @@
+function columnchart(inputJsonFile) {
+    var jsonString = [];
+    var jsonStringName = [];
+    var jsonStringData = [];
+    $.getJSON(inputJsonFile, function(data) {
+
+        for (i = 0; i < data.length; i++) {
+            var mydata={
+                name:data[i].name,
+                y:parseFloat(data[i].y)
+            };
+            jsonString.push(mydata);
+        }
+
+        var processed_json = jsonString
+
+        processed_json = listSortBy(processed_json, 'y', 'desc');
+
+        for (i = 0; i < processed_json.length; i++) {
+            jsonStringName.push(processed_json[i].name);
+            jsonStringData.push(parseFloat(processed_json[i].y));
+        }
+
+        var chart = Highcharts.chart('columnChart', {
+
+            title: {
+                text: 'Column Chart of Topic-Article Distribution'
+            },
+
+
+            xAxis: {
+                categories: jsonStringName
+            },
+
+            series: [{
+                type: 'column',
+                colorByPoint: true,
+                data: jsonStringData,
+                showInLegend: false
+            }]
+
+        });
+    });
+
+}
+
+function listSortBy(arr, field, order){ 
+    var refer = [], result=[], order = order=='asc'?'asc':'desc', index; 
+    for(i=0; i<arr.length; i++){ 
+        refer[i] = arr[i][field]+':'+i; 
+    } 
+    refer.sort(); 
+    if(order=='desc') refer.reverse(); 
+    for(i=0;i<refer.length;i++){ 
+        index = refer[i].split(':')[1]; 
+        result[i] = arr[index]; 
+    } 
+    return result; 
+}
+
+columnchart('article_1_topic20.json');
